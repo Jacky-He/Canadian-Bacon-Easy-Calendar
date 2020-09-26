9411148886bf7ee42b1f9a4fbea9f7d74cea9cfb
alextxu <38475194+alextxu@users.noreply.github.com>
2020-09-26 17:35:30 -0400

    Update backgroundEvent.js

--- includes/js/backgroundEvent.js
+++ includes/js/backgroundEvent.js
@@ -1,5 +1,4 @@
 function bgClick(e) {
-    console.log(e.target.id);
     if (e.target.id != "search-bar" && e.target.id != "dropdown" &&
     e.target.id.substring(0,e.target.id.length - 1) != "suggest") {
         document.getElementById("dropdown").style.display = "none";
