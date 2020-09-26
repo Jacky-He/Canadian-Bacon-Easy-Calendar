7f74eb3f888da33ac75a9e6ad2305430f5de6a27
David Tang <itangdav@gmail.com>
2020-09-25 21:18:12 -0400

    1

--- home.php
+++ home.php
@@ -16,6 +16,22 @@
         <?php include("includes/templates/header.php") ?>
         
         <script>
+            function callFunc (postparams, completion)
+            {
+                let xhttp = new XMLHttpRequest();
+                let formData = new FormData();
+                for (each in postparams) formData.append (each, postparams[each]);
+                xhttp.onreadystatechange = function()
+                {
+                    if (this.readyState == 4 && this.status == 200)
+                    {
+                        completion (this.responseText);
+                    }
+                }
+                xhttp.open("POST", "/data/datafunc.php")
+                xhttp.send(formData);
+            }
+
             var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
             var weekdays = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
@@ -35,17 +51,41 @@
                         type: "OH",
                         start: "9/26/2020 5:30 pm",
                         end: "9/26/2020 6:30 pm",
-                        repeat: false,
+                        repeat: true,
                         repeatday: 1,
                         repeatinterval: 1,
                         zoomlink: "LINK"
                     }
-                ]}
+                ]};
                 if(hasLetters){
-                    //TODO
+                    //Course name
+                    let params = {
+                        funcName: "getCourseByCourseName",
+                        course_name: input
+                    }
+
+                    function setCourse(responseText){
+                        console.log(responseText);
+                        let json = JSON.parse(responseText);
+                        course = json;
+                    }
+
+                    callFunc(params, setCourse);
                 }
                 else{
-                    //TODO
+                    //Course Number
+                    let params = {
+                        funcName: "getCourseByCourseCode",
+                        course_code: input
+                    }
+
+                    function setCourse(responseText){
+                        console.log(responseText);
+                        let json = JSON.parse(responseText);
+                        course = json;
+                    }
+
+                    callFunc(params, setCourse);
                 }
 
                 var cal = ics();
@@ -53,16 +93,16 @@
                 for(var i = 0; i < course["events"].length; i++){
                     var curr = course["events"][i];
                     if(!curr["repeat"]){
-                        cal.addEvent(curr["type"], curr["zoomlink"], "", curr["start"], curr["end"]);
+                        cal.addEvent(curr["type"], curr["zoomlink"], "Carnegie Mellon University", curr["start"], curr["end"]);
                     }
                     else{
                         var rrule = {
                             freq: "WEEKLY",
                             count: 10,
-                            interval: 1,
-                            byday: curr["repeatday"]
+                            interval: curr["repeatinterval"],
+                            byday: [weekdays[curr["repeatday"]]]
                         }
-                        cal.addEvent(curr["type"], curr["zoomlink"], "", curr["start"], curr["end"], rrule);
+                        cal.addEvent(curr["type"], curr["zoomlink"], "Carnegie Mellon University", curr["start"], curr["end"], rrule);
                     }
                 }
 
