3455301914c3ca27e871b46838d940027bb62ee7
Jacky He <jacky.he.juncheng@gmail.com>
2020-09-25 20:20:28 -0400

    commit some

--- index.php
+++ index.php
@@ -4,6 +4,22 @@ if (isset($_SESSION["loggedin"]) && isset($_SESSION["user_id"]) && $_SESSION["lo
 {
     header("location: home.php");
 }
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title> Home Page </title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge;" />
+        <meta name="description" content="">
+        <link rel="stylesheet" href="/includes/styles/css/styles.css">
+        <!-- header start -->
+        <?php include("includes/templates/header.php") ?>
+        <div class="content">
+            
+        </div>
+        <script src="/includes/js/welcome.js"></script>
+        <?php include("includes/templates/footer.php") ?>
+    </body>
+    <!-- footer ends -->
+</html>
