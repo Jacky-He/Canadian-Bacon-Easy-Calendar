64730cc1566057d3e0ecb90123eed12c82929306
David Tang <itangdav@gmail.com>
2020-09-26 13:01:11 -0400

    stuff

--- dashboard.php
+++ dashboard.php
@@ -25,30 +25,30 @@ if (!isset($_SESSION["loggedin"]) || !isset($_SESSION["session_email"]) || $_SES
         <?php include("includes/js/makeics.php") ?>
         <script src="includes/js/search_course.js"></script>
         <div class = "bg">
-        <div class="search">
-            <input id="search-bar" type="text" placeholder="Search Course" onkeydown="processKeyDown()" onclick="processClick()" class="searchCourse"/>
-            <br>
-            <div class="dropdown-content">
-                <p id="suggest0">No course found.</p>
-                <p id="suggest1">C1</p>
-                <p id="suggest2">C2</p>
-                <!-- <p id="suggest3">C3</p>
-                <p id="suggest4">C4</p> -->
+            <div class="search">
+                <input id="search-bar" type="text" placeholder="Search Course" onkeydown="processKeyDown()" onclick="processClick()" class="searchCourse"/>
+                <br>
+                <div class="dropdown-content">
+                    <p id="suggest0">No course found.</p>
+                    <p id="suggest1">C1</p>
+                    <p id="suggest2">C2</p>
+                    <!-- <p id="suggest3">C3</p>
+                    <p id="suggest4">C4</p> -->
+                </div>
             </div>
-        </div>
-        <div class="buttons">     
-        <button onclick="addToICS()">Add to Calendar</button>
-        <button onclick="downloadCal()">Download Calendar</button>
-        <button onclick="removeFromICS()">Remove from Calendar</button>
-        </div>
-        <div class="courseList">
-            <p class = "titleP"> Your Courses </p>
-            <ul id="course-list">
-            <ul>
-        </div>
-        <script>
-            updateList();
-        </script>
+            <div class="buttons">     
+            <button onclick="addToICS()">Add to Calendar</button>
+            <button onclick="downloadCal()">Download Calendar</button>
+            <button onclick="removeFromICS()">Remove from Calendar</button>
+            </div>
+            <div class="courseList">
+                <p class = "titleP"> Your Courses </p>
+                <ul id="course-list">
+                <ul>
+            </div>
+            <script>
+                updateList();
+            </script>
         </div>
     </body>
 </html>
